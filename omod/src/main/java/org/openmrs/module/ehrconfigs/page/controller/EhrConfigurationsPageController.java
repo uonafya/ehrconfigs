ebe7c8f2320c8ab9a6b44fd7bdc7c7be898d7703
injiri <simoninjiri@gmail.com>
2022-03-16 10:25:58 +0300

    moved the waiverType model class to hospitalcore.models

--- omod/src/main/java/org/openmrs/module/ehrconfigs/page/controller/EhrConfigurationsPageController.java
+++ omod/src/main/java/org/openmrs/module/ehrconfigs/page/controller/EhrConfigurationsPageController.java
@@ -2,7 +2,8 @@ package org.openmrs.module.ehrconfigs.page.controller;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.ehrconfigs.models.WaiverType;
+import org.openmrs.module.hospitalcore.BillingService;
+import org.openmrs.module.hospitalcore.model.WaiverType;
 import org.openmrs.module.kenyaui.annotation.AppPage;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
@@ -25,7 +26,14 @@ public class EhrConfigurationsPageController {
         model.addAttribute("selection", selection);
         model.addAttribute("waiverTypes", new ArrayList<WaiverType>());
         model.addAttribute("waiverRoles", Context.getUserService().getAllRoles());
+
+        BillingService billingService = Context.getService(BillingService.class);
+        WaiverType waiverType = new WaiverType();
+        waiverType.setName("Hospital Waiver");
+        waiverType.setUserRole(Context.getUserService().getRole("EHR Edit Bill"));
+        waiverType.setDescription("Clients exempted from paying any services at the facility");
+        billingService.saveWaiverType(waiverType);
+        System.out.println(waiverType);
+
     }
-
-
 }
