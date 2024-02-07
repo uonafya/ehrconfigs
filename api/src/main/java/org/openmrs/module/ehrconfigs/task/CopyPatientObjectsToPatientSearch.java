eb364a1570696ccfad0912731566289c582d7b22
Nicholas Ingosi <nicotwendelee@gmail.com>
2024-02-07 12:41:19 +0300

    Add mechanisms to allow patient setup at the start for the data migration

--- api/src/main/java/org/openmrs/module/ehrconfigs/task/CopyPatientObjectsToPatientSearch.java
+++ api/src/main/java/org/openmrs/module/ehrconfigs/task/CopyPatientObjectsToPatientSearch.java
@@ -15,6 +15,9 @@ package org.openmrs.module.ehrconfigs.task;
 
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifier;
+import org.openmrs.PersonAttribute;
+import org.openmrs.PersonAttributeType;
+import org.openmrs.api.PersonService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hospitalcore.HospitalCoreService;
 import org.openmrs.module.hospitalcore.model.PatientSearch;
@@ -23,6 +26,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 public class CopyPatientObjectsToPatientSearch extends AbstractTask {
@@ -41,6 +45,9 @@ public class CopyPatientObjectsToPatientSearch extends AbstractTask {
 			try {
 				//do all the work here
 				/*for (Patient patient : patientList) {
+					//set person attribute to allow searching of patient
+					generatePersonalAttributeForPatients(patient);
+					//save the record into the patient search for easier navigation
 					copyPatientObjectsToPatientSearchObjects(patient);
 				}*/
 			}
@@ -92,6 +99,34 @@ public class CopyPatientObjectsToPatientSearch extends AbstractTask {
 				log.info("Saved patient object successfully>> " + patient);
 			}
 			
+		}
+	}
+	private void generatePersonalAttributeForPatients(Patient patient) {
+		PersonService personService = Context.getPersonService();
+		PersonAttributeType paymentCategory = personService.getPersonAttributeTypeByUuid("09cd268a-f0f5-11ea-99a8-b3467ddbf779");
+		PersonAttributeType patientCategory = personService.getPersonAttributeTypeByUuid("972a32aa-6159-11eb-bc2d-9785fed39154");
+
+		if(patient != null && patient.getAttribute(paymentCategory) == null ) {
+			PersonAttribute personAttributePayingCategory = new PersonAttribute();
+			personAttributePayingCategory.setAttributeType(paymentCategory);
+			personAttributePayingCategory.setPerson(patient);
+			personAttributePayingCategory.setValue("Paying");
+			personAttributePayingCategory.setCreator(Context.getAuthenticatedUser());
+			personAttributePayingCategory.setDateCreated(new Date());
+			patient.addAttribute(personAttributePayingCategory);
+			personService.savePerson(patient);
+			System.out.println("Saved the person attribute - paying category");
+
+
+			PersonAttribute personAttributePatientCategory = new PersonAttribute();
+			personAttributePatientCategory.setAttributeType(patientCategory);
+			personAttributePatientCategory.setPerson(patient);
+			personAttributePatientCategory.setValue("General Patient");
+			personAttributePatientCategory.setCreator(Context.getAuthenticatedUser());
+			personAttributePatientCategory.setDateCreated(new Date());
+			patient.addAttribute(personAttributePatientCategory);
+			personService.savePerson(patient);
+			System.out.println("Saved the person attribute - patient category");
 		}
 	}
 }
